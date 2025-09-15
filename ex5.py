bc9088dbe283b45222d3f6676a4cf3f42fb5905f
Listwas <listwa.patrykkr@gmail.com>
2025-09-15 15:22:28 +0200

    solved

---
+++ ex5.py
@@ -0,0 +1,17 @@
+#Write a code to return True if the list’s first and last numbers are the same. If the numbers are different, return False.
+
+numbers_x = [10, 20, 30, 40, 10]
+numbers_y = [75, 65, 35, 75, 30]
+
+def check_list(x):
+    print("your list: ", x)
+    first = x[0]
+    last = x[-1]
+    
+    if first == last:
+        return True
+    else:
+        return False
+
+print(check_list(numbers_x))
+print(check_list(numbers_y))
