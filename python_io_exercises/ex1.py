a70a1c2210d80c9e60f9c2cab7d82c4c09f3b570
Listwas <listwa.patrykkr@gmail.com>
2025-09-19 13:06:31 +0200

    solved

---
+++ python_io_exercises/ex1.py
@@ -0,0 +1,6 @@
+#Write a program to accept two integer numbers from the user and calculate their product.
+
+num1 = int(input("first num: "))
+num2 = int(input("second num: "))
+
+print("res: ",num1 * num2)
---
+++ python_io_exercises/ex3.py
@@ -0,0 +1,8 @@
+#Display Decimal Number to Octal using print() function
+
+num = 8
+
+print('%o' % num)
+print(f'{num:o}')
+print('{:o}'.format(num))
+print(format(num, 'o'))
