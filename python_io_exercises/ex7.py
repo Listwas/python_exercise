2b2c7bbb6e4b7dc300f05a8e4c1a84f044de472a
Listwas <listwa.patrykkr@gmail.com>
2025-09-19 14:10:00 +0200

    solved

---
+++ python_io_exercises/ex7.py
@@ -0,0 +1,5 @@
+#Accept any three string from one input() call
+
+i1, i2, i3 = input("num: ").split()
+
+print(i1, i2, i3)
---
+++ python_io_exercises/ex8.py
@@ -0,0 +1,17 @@
+#Format variables using string.format() method
+#Write a program to use the string.format() method to format the following three variables according to the expected output.
+
+#Given:
+#totalMoney = 1000
+#quantity = 3
+#price = 450
+
+#Expected Output:
+#I have 1000 dollars so I can buy 3 football for 450.00 dollars.
+
+totalMoney = 1000
+quantity = 3
+price = 450
+string = "I have {0} dollars so I can buy {1} football for {2} dollars."
+
+print(string.format(totalMoney, quantity, price))
