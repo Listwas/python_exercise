acf7add5566faeca447d0a360b66406aabf0ca87
Listwas <listwa.patrykkr@gmail.com>
2025-09-21 19:02:23 +0200

    solved*

---
+++ python_if_else_loop/ex18.py
@@ -0,0 +1,19 @@
+#Write a program to print the following start pattern using the for loop
+#
+#* 
+#* * 
+#* * * 
+#* * * * 
+#* * * * * 
+#* * * * 
+#* * * 
+#* * 
+#*
+
+for i in range(6):
+    print("*" * i)
+
+for j in range(4, 0, -1):
+    print("*" * j)
+
+    
