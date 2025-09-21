7820cec6def4a39555b7d076216e0687393b61a3
Listwas <listwa.patrykkr@gmail.com>
2025-09-21 15:34:53 +0200

    solved

---
+++ python_if_else_loop/ex8.py
@@ -0,0 +1,9 @@
+#Print list in reverse order using a loop
+
+list1 = [10, 20, 30, 40, 50]
+
+for number in reversed(list1):
+    print(number)
+
+#for i in range(len(list1)-1, -1, -1):
+#    print(list1[i])
